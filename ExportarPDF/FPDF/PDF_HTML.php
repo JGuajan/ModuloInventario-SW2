c625acaf5aaee7d11c7ebe6a8b7a8595cdef68dc
catoapanta <megacriszero@gmail.com>
2017-07-23 09:56:46 -0500

    Diseño del PDF

--- ExportarPDF/FPDF/PDF_HTML.php
+++ ExportarPDF/FPDF/PDF_HTML.php
@@ -279,6 +279,99 @@ function PutLink($URL, $txt)
     $this->SetTextColor(0);
 }
 
+function CellFit($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false, $link='', $scale=false, $force=true)
+    {
+        //Get string width
+        $str_width=$this->GetStringWidth($txt);
+ 
+        //Calculate ratio to fit cell
+        if($w==0)
+            $w = $this->w-$this->rMargin-$this->x;
+        $ratio = ($w-$this->cMargin*2)/$str_width;
+ 
+        $fit = ($ratio < 1 || ($ratio > 1 && $force));
+        if ($fit)
+        {
+            if ($scale)
+            {
+                //Calculate horizontal scaling
+                $horiz_scale=$ratio*100.0;
+                //Set horizontal scaling
+                $this->_out(sprintf('BT %.2F Tz ET',$horiz_scale));
+            }
+            else
+            {
+                //Calculate character spacing in points
+                $char_space=($w-$this->cMargin*2-$str_width)/max($this->MBGetStringLength($txt)-1,1)*$this->k;
+                //Set character spacing
+                $this->_out(sprintf('BT %.2F Tc ET',$char_space));
+            }
+            //Override user alignment (since text will fill up cell)
+            $align='';
+        }
+ 
+        //Pass on to Cell method
+        $this->Cell($w,$h,$txt,$border,$ln,$align,$fill,$link);
+ 
+        //Reset character spacing/horizontal scaling
+        if ($fit)
+            $this->_out('BT '.($scale ? '100 Tz' : '0 Tc').' ET');
+    }
+    
+    
+    function CellFitSpace($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false, $link='')
+    {
+        $this->CellFit($w,$h,$txt,$border,$ln,$align,$fill,$link,false,false);
+    }
+
+    function MBGetStringLength($s)
+    {
+        if($this->CurrentFont['type']=='Type0')
+        {
+            $len = 0;
+            $nbbytes = strlen($s);
+            for ($i = 0; $i < $nbbytes; $i++)
+            {
+                if (ord($s[$i])<128)
+                    $len++;
+                else
+                {
+                    $len++;
+                    $i++;
+                }
+            }
+            return $len;
+        }
+        else
+            return strlen($s);
+    }
+    
+    function cabeceraVertical($cabecera)
+    {
+        $this->SetXY(10, 10); //Seleccionamos posición
+        $this->SetFont('Arial','B',10); //Fuente, Negrita, tamaño
+ 
+        foreach($cabecera as $columna)
+        {
+            //Parámetro con valor 2, cabecera vertical
+            $this->Cell(30,7, utf8_decode($columna),1, 2 , 'L' );
+        }
+    }
+ 
+    function cabeceraHorizontal($cabecera)
+    {
+        $this->SetXY(10, 70);
+        $this->SetFont('Arial','B',10);
+        foreach($cabecera as $fila)
+        {
+            //Atención!! el parámetro valor 0, hace que sea horizontal
+            $this->Cell(24,7, utf8_decode($fila),1, 0 , 'L' );
+        }
+    }
+    
+//************** Fin del código para ajustar texto *****************
+//******************************************************************
+    
 }//end of class
 
 ?>
--- ExportarPDF/pdf_exportar.php
+++ ExportarPDF/pdf_exportar.php
@@ -4,60 +4,127 @@ session_start();
 include_once '../Model/AjusteDet.php';
 include_once '../Model/AjustesModel.php';
 
-
- 
-       
 require ('FPDF/PDF_HTML.php');
 
-$pdf = new PDF_HTML();
+$pdf = new PDF_HTML(); 
 $pdf->AddPage();
 $pdf->SetFont('Arial');
+
 $pdf->Image('img/Titulo_X2.jpg', 3, 0, 200, 57);
 $pdf->Ln(50);
 $pdf->Write(1, '_______________________________________________________________________________');
-$pdf->Ln(13);
+$pdf->Ln(10);
+$pdf->SetFillColor(2, 152, 116);
 $pdf->Cell(0, 9, 'CABECERA DEL AJUSTE', 0, 0, 'C', 0);
 $pdf->Ln(15);
-$pdf->Cell(0, 9, 'Codigo del Ajuste : ' . $_SESSION['cod'], 0, 0, 'L', 0);
-$pdf->Ln(8);
-$pdf->Cell(0, 9, 'Motivo : ' . $_SESSION['mot'], 0, 0, 'L', 0);
-$pdf->Ln(13);
+
+$pdf->SetTextColor(240, 255, 240);
+$pdf->CellFitSpace(95, 6, 'CODIGO DEL AJUSTE' , 1 , 0, 'C', 1);
+$pdf->SetTextColor(3, 3, 3);
+$pdf->CellFitSpace(95, 6, $_SESSION['cod'] , 1 , 0, 'C');
+$pdf->Ln(6);
+
+$pdf->SetTextColor(240, 255, 240);
+$pdf->CellFitSpace(95, 6, 'MOTIVO DEL AJUSTE' , 1 , 0, 'C', 1);
+$pdf->SetTextColor(3, 3, 3);
+$pdf->CellFitSpace(95, 6, $_SESSION['mot'] , 1 , 0, 'C');
+$pdf->Ln(6);
+
+$pdf->Ln(15);
+
 $pdf->Write(1, '_______________________________________________________________________________');
-$pdf->Ln(13);
+$pdf->Ln(15);
 $pdf->Cell(0, 9, 'DETALLES DEL AJUSTE', 0, 0, 'C', 0);
 $pdf->Ln(15);
 $pdf->Write(1, '_______________________________________________________________________________');
 $pdf->Ln(15);
 
+$pdf->SetTextColor(240, 255, 240);
+$pdf->CellFitSpace(40, 6, 'CODIGO DEL DETALLE' , 1 , 0, 'C', 1);
+$pdf->CellFitSpace(50, 6, 'NOMBRE DEL PRODUCTO' , 1 , 0, 'C', 1 );
+$pdf->CellFitSpace(25, 6, 'CANTIDAD' , 1 , 0, 'C', 1) ;
+$pdf->CellFitSpace(25, 6, 'ACCION' , 1 , 0, 'C', 1);
+$pdf->CellFitSpace(25, 6, 'PRECIO' , 1 , 0, 'C', 1);
+$pdf->CellFitSpace(25, 6, 'GRABA IVA' , 1 , 0, 'C', 1);
+$pdf->Ln(6);
+
+$bandera = false;
+
 if (isset($_SESSION['listadoDetAjuste'])) {
-    // Deserializamos y mostraremos los atributos de los usuarios usando un ciclo for
+   
     $listado = unserialize($_SESSION['listadoDetAjuste']);
     
     foreach ($listado as $aju) {
        
+        $detalle=$aju->getID_DETALLE_AJUSTE_PROD();
+        $nombre=$aju->getNOMBRE_PROD();
+        $stock=$aju->getCAMBIO_STOCK_PROD();
+        
         if($aju->getTIPOMOV_DETAJUSTE_PROD()=='E'){
             $tipomov='Entrada';
         }else{
              $tipomov='Salida';
         }
         
+        $pvp=$aju->getPVP_PROD();
+        
         if($aju->getGRABA_IVA_PROD()=='S'){
             $iva='Si';
         }else{
             $iva='No';
         }
-        $nomprod=$aju->getNOMBRE_PROD();
-        $FILADET= $aju->getID_DETALLE_AJUSTE_PROD()."   ".$nomprod."<--->".$aju->getCAMBIO_STOCK_PROD()
-                   ."<------>".$tipomov."<------>".$aju->getPVP_PROD()."<---->".$iva;
-        $pdf->Cell(0, 0,$FILADET , 0, 0, 'L', 0);
-        $pdf->Ln(5);
-    }
+         
+        $pdf->SetTextColor(3, 3, 3);
+        $pdf->SetFillColor(229, 229, 229);
+        
+        $pdf->CellFitSpace(40, 6, $detalle, 1 , 0, 'C', $bandera);
+        $pdf->CellFitSpace(50, 6, $nombre, 1 , 0, 'C', $bandera);
+        $pdf->CellFitSpace(25, 6, $stock, 1 , 0, 'C', $bandera) ;
+        $pdf->CellFitSpace(25, 6, $tipomov, 1 , 0, 'C', $bandera);
+        $pdf->CellFitSpace(25, 6, $pvp , 1 , 0, 'C', $bandera);
+        $pdf->CellFitSpace(25, 6, $iva, 1 , 0, 'C', $bandera);
+        $pdf->Ln(6);
+        
+        $bandera=!$bandera;
+    
+        
+        }
   
 }else{
      $pdf->Cell(0, 0,'no', 0, 0, 'L', 0);
 }
- $pdf->Write(1, '_______________________________________________________________________________');
+$pdf->Ln(10);
+$pdf->Write(1, '_______________________________________________________________________________');
 $pdf->Output();
+//if (isset($_SESSION['listadoDetAjuste'])) {
+//    // Deserializamos y mostraremos los atributos de los usuarios usando un ciclo for
+//    $listado = unserialize($_SESSION['listadoDetAjuste']);
+//    
+//    foreach ($listado as $aju) {
+//       
+//        if($aju->getTIPOMOV_DETAJUSTE_PROD()=='E'){
+//            $tipomov='Entrada';
+//        }else{
+//             $tipomov='Salida';
+//        }
+//        
+//        if($aju->getGRABA_IVA_PROD()=='S'){
+//            $iva='Si';
+//        }else{
+//            $iva='No';
+//        }
+//        $nomprod=$aju->getNOMBRE_PROD();
+//        $FILADET= $aju->getID_DETALLE_AJUSTE_PROD()."   ".$nomprod."<--->".$aju->getCAMBIO_STOCK_PROD()
+//                   ."<------>".$tipomov."<------>".$aju->getPVP_PROD()."<---->".$iva;
+//        $pdf->Cell(0, 0,$FILADET , 0, 0, 'L', 0);
+//        $pdf->Ln(5);
+//    }
+//  
+//}else{
+//     $pdf->Cell(0, 0,'no', 0, 0, 'L', 0);
+//}
+// $pdf->Write(1, '_______________________________________________________________________________');
+////$pdf->Output();
 
 ?>
 
