c74fee3e7e5558063d82411c3556153f714f3aeb
rftarupi <rick030gp@gmail.com>
2017-07-30 19:14:49 -0500

    Prueba 2

--- Controller/controller.php
+++ Controller/controller.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../model/UsuariosModel.php';
-require_once '../model/AjustesModel.php';
-require_once '../Model/ProductosModel.php';
+require_once '/../model/UsuariosModel.php';
+require_once '/../model/AjustesModel.php';
+require_once '/../Model/ProductosModel.php';
 
 session_start();
 $usuariosModel = new UsuariosModel();
